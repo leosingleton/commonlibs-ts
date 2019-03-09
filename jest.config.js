833eb4121c9510346e7846c5c3baca1376259b2f
Leo Singleton <leo@leosingleton.com>
2019-03-09 11:54:16 -0500

    Enabling code coverage
    To avoid directory spawl and a longer .gitignore, I moved the output to
    build/dist/ so build/coverage/ could be used for code coverage results.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  roots: ["<rootDir>/src/"]
+  roots: ['<rootDir>/src/'],
+  collectCoverage: true,
+  coverageDirectory: 'build/coverage/'
 };
