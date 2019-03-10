a68d0b8859b782e32516b0c3909347c8a640adac
Leo Singleton <leo@leosingleton.com>
2019-03-10 14:39:15 -0400

    Adding Task.run() implementation

--- src/dotnet/Task.ts
+++ src/dotnet/Task.ts
@@ -9,4 +9,33 @@ export class Task {
     let event = new AsyncTimerEvent(millisecondsDelay);
     return event.waitAsync();
   }
+
+  /**
+   * Executes a lambda function asynchronously. Equivalent to the proposed but never implemented setImmediate()
+   * function in JavaScript.
+   * @param lambda Lambda function to execute
+   */
+  public static run(lambda: Lambda): void {
+    // window.postMessage() is the fastest method according to http://ajaxian.com/archives/settimeout-delay
+    readyTasks.push(lambda);
+    window.postMessage(eventData, '*');
+  }
 }
+
+type Lambda = () => void;
+const eventData = '@ls/cl/T.r'; // Any unique string. Abbreviated version of "@leosingleton/commonlibs-ts/Task.run"
+let readyTasks: Lambda[] = [];
+
+window.addEventListener('message', event => {
+  if (event.data === eventData) {
+    event.stopPropagation();
+    while (readyTasks.length > 0) {
+      let lambda = readyTasks.shift();
+      try {
+        lambda();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+}, true);
---
+++ src/dotnet/__tests__/Task.test.ts
@@ -0,0 +1,29 @@
+import { Task } from '../Task';
+
+describe('Task', () => {
+
+  it('Task.run() executes asynchronously', async () => {
+    let n = 1;
+
+    function f() {
+      Task.run(() => {
+        while (n < 100) {
+          n += 1;
+          f();  
+        }
+      });
+    }
+
+    f();
+
+    // The value should still be 1 because the increment happens asynchronously
+    expect(n).toEqual(1);
+
+    // Give the tasks some time to execute
+    await Task.delay(100);
+
+    // All 100 iterations should complete in a few milliseconds
+    expect(n).toEqual(100);
+  });
+
+});
