268526ce4a83516b9fdd9c7ba5c3e59f1fd90666
Leo Singleton <leo@leosingleton.com>
2019-03-10 16:17:09 -0400

    Reimplementing Task.delay() to use Promises directly

--- src/dotnet/Task.ts
+++ src/dotnet/Task.ts
@@ -1,13 +1,16 @@
-import { AsyncTimerEvent } from '../coordination/AsyncTimerEvent';
-
 export class Task {
   /**
    * Blocks the current execution for the specified number of milliseconds. Equivalent to Task.Delay() in C#.
    * @param millisecondsDelay Number of milliseconds to delay
    */
   public static delay(millisecondsDelay: number): Promise<void> {
-    let event = new AsyncTimerEvent(millisecondsDelay);
-    return event.waitAsync();
+    let onResolve: (value?: void) => void;
+
+    setTimeout(() => onResolve(), millisecondsDelay);
+
+    return new Promise<void>((resolve, reject) => {
+      onResolve = resolve;
+    });
   }
 
   /**
