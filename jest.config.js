1f75426b2d482bf0647c183534b3bc1569d3a71b
Leo Singleton <leo@leosingleton.com>
2019-03-10 17:43:53 -0400

    Using jest-junit to collect unit tests results for Azure Pipelines

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src/'],
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'build/',
+      outputName: './results.xml',
+    }]
+  ],
   collectCoverage: true,
   coverageDirectory: 'build/coverage/'
 };
