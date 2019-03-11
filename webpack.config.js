219d04e9458721d5256561158b5864765c733a5a
Leo Singleton <leo@leosingleton.com>
2019-03-10 23:47:32 -0400

    Using Webpack to build a single JS file as output

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+
+var config = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts' ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build/dist'),
+    filename: 'index.js',
+    library: 'library',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  }
+};
+
+module.exports = config;
