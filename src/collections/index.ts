cb2546078fdb04c177e412ec41ba932ea96d343b
Leo Singleton <leo@leosingleton.com>
2019-03-16 22:27:40 +0530

    Exporting CircularBuffer and PriorityQueue classes

--- src/collections/index.ts
+++ src/collections/index.ts
@@ -2,5 +2,7 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
+export * from './CircularBuffer';
 export * from './DisposableSet';
 export * from './MovingAverage';
+export * from './PriorityQueue';
