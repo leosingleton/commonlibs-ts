af0b49cc2d16a209231325e4ea64e1591f3feaaa
Leo Singleton <leo@leosingleton.com>
2019-03-22 11:40:16 +0530

    Adding comments and unit tests for Queue<T>

--- src/dotnet/Queue.ts
+++ src/dotnet/Queue.ts
@@ -2,30 +2,51 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
+/**
+ * First-In First-Out Queue. Mirror's the class in .NET for code portability.
+ */
 export class Queue<T> {
+  /**
+   * Enqueues a value
+   * @param value Value
+   */
   public enqueue(value: T) {
     this._values.push(value);
   }
 
   /**
-   * @returns Last value in the queue, or undefined if the queue is empty
+   * Removes and returns the value at the head of the queue
+   * @returns First value in the queue, or undefined if the queue is empty
    */
   public dequeue(): T | undefined {
     return this._values.shift();
   }
 
+  /**
+   * Returns the value at the head of the queue without removing it
+   * @returns First value in the queue, or undefined if the queue is empty
+   */
   public tryPeek(): T | undefined {
     return this._values[0];
   }
 
+  /**
+   * Returns the number of values in the queue
+   */
   public getCount(): number {
     return this._values.length;
   }
 
+  /**
+   * Returns the queue as an array
+   */
   public toArray(): T[] {
     return this._values;
   }
 
+  /**
+   * Returns true if the queue is empty; false otherwise
+   */
   public isEmpty(): boolean {
     return (this._values.length === 0);
   }
---
+++ src/dotnet/__tests__/Queue.test.ts
@@ -0,0 +1,33 @@
+// @leosingleton/commonlibs - Common Libraries for TypeScript and .NET Core
+// Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
+// See LICENSE in the project root for license information.
+
+import { Queue } from '../Queue';
+
+describe('Queue', () => {
+
+  it('Enqueues and dequeues values', () => {
+    let q = new Queue<number>();
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    expect(q.getCount()).toEqual(3);
+    expect(q.isEmpty()).toBeFalsy();
+    expect(q.toArray()).toEqual([1, 2, 3]);
+
+    expect(q.tryPeek()).toEqual(1);
+    expect(q.dequeue()).toEqual(1);
+    expect(q.tryPeek()).toEqual(2);
+    expect(q.dequeue()).toEqual(2);
+    expect(q.tryPeek()).toEqual(3);
+    expect(q.dequeue()).toEqual(3);
+    expect(q.tryPeek()).toBeUndefined();
+    expect(q.dequeue()).toBeUndefined();
+
+    expect(q.getCount()).toEqual(0);
+    expect(q.isEmpty()).toBeTruthy();
+  });
+
+});
