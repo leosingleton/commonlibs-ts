9566fb0c4f9cfc72dfeb3505f15790ae7e8c1013
Leo Singleton <leo@leosingleton.com>
2019-03-22 15:27:52 +0530

    Re-adding unit tests for NodeJS
    After a bit more trial-and-error, the factor causing test failures
    appears to be single-core machines. It's not clear why this is the
    case, but hardcoding maxWorkers=2 on all NodeJS automation runs to
    avoid hitting the bug.

--- jest.node.config.js
+++ jest.node.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src/collections', '<rootDir>/src/logic', '<rootDir>/src/math'],
+  roots: ['<rootDir>/src/'],
   reporters: [
     'default',
     ['jest-junit', {
