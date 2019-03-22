765d939a4db7c81e0dfef1bbe0034170be1c006f
Leo Singleton <leo@leosingleton.com>
2019-03-22 07:59:16 +0530

    Excluding more tests from NodeJS

--- jest.node.config.js
+++ jest.node.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src/collections', '<rootDir>/src/dotnet', '<rootDir>/src/logic', '<rootDir>/src/math'],
+  roots: ['<rootDir>/src/math'],
   reporters: [
     'default',
     ['jest-junit', {
