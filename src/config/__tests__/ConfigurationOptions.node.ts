b504cfd7d26c6a1a1fda0fbb2e2577a508d07da6
Leo Singleton <leo@leosingleton.com>
2019-08-07 21:06:14 -0400

    Adding Config class from WhiteboardCam

---
+++ src/config/__tests__/ConfigurationOptions.node.ts
@@ -0,0 +1,21 @@
+// @leosingleton/commonlibs - Common Libraries for TypeScript and .NET Core
+// Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
+// See LICENSE in the project root for license information.
+
+import { SampleConfig } from './SampleConfig';
+
+describe('ConfigurationOptions', () => {
+
+  it('Populates with defaults', () => {
+    let config = new SampleConfig();
+    expect(config.localString).toBe('Hello');
+    expect(config.sessionNumber).toBe(42);
+    expect(config.noneBoolean).toBeTruthy();
+  });
+
+  it('Throws an exception if writing from NodeJS', () => {
+    let config = new SampleConfig();
+    expect(() => config.writeToStorage({ localString: 'New String' })).toThrowError();
+  });
+
+});
---
+++ src/config/__tests__/SampleConfig.ts
@@ -0,0 +1,28 @@
+// @leosingleton/commonlibs - Common Libraries for TypeScript and .NET Core
+// Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
+// See LICENSE in the project root for license information.
+
+import { ConfigurationOptions, ConfigurationFlags, StorageType } from '../ConfigurationOptions';
+
+/** Sample class demonstrating the use of ConfigurationOptions */
+export class SampleConfig extends ConfigurationOptions {
+  public constructor() {
+    super('test_', ConfigurationFlags.AllowNonBrowsers);
+    this.initialize();
+  }
+
+  /** A string setting persisted in local storage */
+  public readonly localString: string;
+
+  /** A numeric setting persisted in session storage */
+  public readonly sessionNumber: number;
+
+  /** A boolean setting not persisted */
+  public readonly noneBoolean: boolean;
+
+  protected defaults: {[id: string]: [string, StorageType, any]} = {
+    localString: ['str', StorageType.Local, 'Hello'],
+    sessionNumber: ['num', StorageType.Session, 42],
+    noneBoolean: ['bool', StorageType.None, true]
+  };
+}
