bd3e90c1459f8397b51567ed85aa57e7c743963a
Leo Singleton <leo@leosingleton.com>
2020-05-14 07:20:11 -0400

    CallbackCollection to use new CallbackMaybeAsync

--- src/collections/CallbackCollection.ts
+++ src/collections/CallbackCollection.ts
@@ -2,7 +2,7 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
-import { UnhandledError } from '../js/UnhandledError';
+import { CallbackMaybeAsync, executeCallbacks } from '../js/Callback';
 
 /** Helper class to register and invoke callback functions */
 export class CallbackCollection {
@@ -10,19 +10,13 @@ export class CallbackCollection {
    * Registers a callback to invoke in `invokeCallbacks()`
    * @param callback Callback to invoke
    */
-  public registerCallback(callback: () => void): void {
+  public registerCallback(callback: CallbackMaybeAsync): void {
     this.callbacks.push(callback);
   }
 
   /** Invokes all registered callback functions */
   public invokeCallbacks(): void {
-    for (const callback of this.callbacks) {
-      try {
-        callback();
-      } catch (err) {
-        UnhandledError.reportError(err);
-      }
-    }
+    executeCallbacks(this.callbacks);
   }
 
   /** Removes all registered callback functions */
@@ -31,5 +25,5 @@ export class CallbackCollection {
   }
 
   /** Registered callback functions */
-  private callbacks: (() => void)[] = [];
+  private callbacks: CallbackMaybeAsync[] = [];
 }
